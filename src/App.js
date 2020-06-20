8ec8a7d367806e2def8e2a14705bcd50d923373f
junho <junho313@naver.com>
2020-06-20 17:54:07 +0900

    propTypes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,71 @@
 import React from 'react';
+import ProptTypes from 'prop-types';
+
 
 //인자가 오브젝트로 넘어오기 때문에
 //사용 할때 props.변수명 이나
 //인자를 받을 때 {}(오브젝트 사용 의미) 를 사용하여 {변수명} 을 하면 된다
 //props(속성값)
-function Food({name, image}) {
+function Food({name, image, rating}) {
   //console.log(props.fav);
   //console.log(fav);
   return (
     <div>
       <h2>I like {name}.</h2>
+      <h4>{rating} / 5.0</h4>
       <img src={image} alt={name}/>
     </div>
   );
 }
 
+Food.propTypes = {
+  //타입체크
+  name: ProptTypes.string.isRequired,
+  image: ProptTypes.string.isRequired,
+  rating: ProptTypes.number
+  //isRequired 은 필수인지 아닌지, 없으면 인자가 없어도 된다
+};
+
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.8
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5.0
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.7
   }
 ];
 
 function renderFood(dish) {
   //console.log(dish);
-  return <Food name={dish.name} image={dish.image}/>
+  return <Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating}/>
 }
 
 function App() {
